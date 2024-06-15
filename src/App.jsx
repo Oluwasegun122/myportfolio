43063c36cad1872675d6294544e691b9b6cec690
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2024-06-15 12:05:45 +0100

    added my portfolio site file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/output.css'; // Import the generated CSS
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'sm': '380px'
+      },
+      spacing:{
+        "big":"48rem"
+      },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      height: {  // Use `height` for custom height values
+        "siz": "500px"
+      },
+      width: {  // Optionally, use `width` for custom width values if needed
+        "sizw": "600px"
+      },
+    },
+  },
+  plugins: [],
+  content: [
+    './**/*.html',
+    './public/src/**/*.js',
+  ],
+};
